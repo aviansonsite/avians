e45577da8c61ae2836b9536987fa36fdc56eb6b8
Omkar Mankar <omkar9497@gmail.com>
2023-08-28 13:15:15 +0530

    d

--- app/Http/Controllers/LabourAPIController.php
+++ app/Http/Controllers/LabourAPIController.php
@@ -364,11 +364,12 @@ class LabourAPIController extends Controller
                                 // $image_id= uniqid();
                                 $filename= $photo_path_ext."_".md5($photo_path. microtime()).'.'.$photo_path_ext;
 
-                                file_put_contents($destinationPath.$filename, $data);                                
+                                file_put_contents($destinationPath.$filename, $data);
+                                $u_obj->pout_img=$fileName;                                
                             }
 
 
-                        $u_obj->pout_img=$fileName;
+                        
                         $res=$u_obj->save();
                     }else{
                         // Session::put('ERROR_MESSAGE', 'Something went wrong. Please try again.');
